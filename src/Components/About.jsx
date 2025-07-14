3f080f79018bdf0b95c18bf581dc9a50ff30a74a
pushpendra <singh16195@gmail.com>
2025-07-14 22:48:07 +0530

    user component integration

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -1,11 +1,14 @@
 import Header from "./Header";
 import Footer from "./Footer";
+import User from "./User";
 
 export default function About() {
   return (
     <>
       <Header />
-      <p>About Component</p>
+
+      <User />
+
       <Footer />
     </>
   );
